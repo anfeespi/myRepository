b1416c5fe8e888a1bcf9124c49cb7af966fd0bc9
anfeespi <122488352+anfeespi@users.noreply.github.com>
2023-06-18 15:25:05 -0500

    Delete MyProjects/CarrenoJuan_Prog2 directory

--- MyProjects/CarrenoJuan_Prog2/src/main/java/co/edu/unbosque/CarrenoJuan_Prog2/repository/AnimalRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.unbosque.CarrenoJuan_Prog2.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import co.edu.unbosque.CarrenoJuan_Prog2.model.Animal;
-
-public interface AnimalRepository extends CrudRepository<Animal, Integer>{
-
-}
