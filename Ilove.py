05049649c4639ccfdbf81b820994003a1c2d9a29
anfeespi <122488352+anfeespi@users.noreply.github.com>
2024-05-26 13:41:07 -0500

    Delete Ilove.py

--- Ilove.py
+++
@@ -1,8 +0,0 @@
-n, d = input().split()
-
-if int(n[len(n)-1]) > int(d):
-    print((int(n) + 10) - (int(n[len(n)-1]) - int(d)))
-elif int(n[len(n)-1]) < int(d):
-    print(int(n) + (int(d) - int(n[len(n) - 1])))
-else:
-    print(int(n) + 10)
